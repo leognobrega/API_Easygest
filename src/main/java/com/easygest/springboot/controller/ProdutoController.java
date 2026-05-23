a56b99145088f1b46c47da624756c627b9e79afa
leognobrega <leognobrega>
2026-05-23 12:28:05 -0300

    Inclusão de Excluir

--- src/main/java/com/easygest/springboot/controller/ProdutoController.java
+++ src/main/java/com/easygest/springboot/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,4 +37,9 @@ public class ProdutoController {
     public Produto atualizar(@RequestBody Produto produto, @PathVariable Long codProduto){
         return produtoService.atualizar(codProduto, produto);
     }
+
+    @DeleteMapping("/{codProduto}")
+public void excluir(@PathVariable Long codProduto) {
+    produtoService.excluir(codProduto);
+}
 }
